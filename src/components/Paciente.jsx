2b9090a987602d0fa5c1ddb1ae7c868f9acd1b5a
Heartness <eddie.edward97@gmail.com>
2023-02-22 19:42:21 -0600

    Visualización de pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,27 @@
+function Paciente() {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+          <span className="font-normal normal-case">Juan</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">E-mail: {''}
+          <span className="font-normal normal-case">correo@correo.com</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de Alta: {''}
+          <span className="font-normal normal-case">22/02/2023</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+          <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore architecto harum repellendus dolorem in dignissimos ipsum minima temporibus dicta necessitatibus. Sapiente fugiat excepturi cumque nemo eius, perspiciatis recusandae. Repudiandae, ratione?</span>
+        </p>
+      </div>
+  )
+}
+
+export default Paciente
