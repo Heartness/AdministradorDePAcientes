dba4bd481d5a54e0b367ad07518ca06521250dfe
Heartness <eddie.edward97@gmail.com>
2023-02-24 21:01:02 -0600

    Debugging

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -26,7 +26,7 @@ function ListadoPacientes({ pacientes, setPaciente, eliminarPaciente }) {
           <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
 
           <p className="text-xl mt-5 mb-10 text-center">
-            Comienza a agregar pcientes {""}
+            Comienza a agregar pacientes {""}
             <span className="text-indigo-600 font-bold">y aparecerán aquí</span>
           </p>
         </>
